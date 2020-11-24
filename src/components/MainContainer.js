aa05e5b26407f55f241bc565870000a8e4b6e8ac
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-11-24 15:33:42 -0500

    maincontainer component

---
+++ src/components/MainContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MainContainer = () => {
+    return (<div>
+        <h1 className='text-center'>Kelsey Shiba</h1>
+    </div>)
+}
+
+export default MainContainer;
