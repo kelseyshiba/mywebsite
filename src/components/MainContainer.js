794d1c1086256b9d52ece96cb7bd966536f275b6
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-11-25 12:34:48 -0500

    main and navbar updates with React Bootstrap

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 
 const MainContainer = () => {
-    return (<div>
-        <h1 className='text-center'>Kelsey Shiba</h1>
-    </div>)
+    return (
+        <div>
+        <header>
+            <h1 className='text-center'>Kelsey Shiba</h1>
+        </header>
+        <Container className='justify-content-center'>
+        <Row>
+            <Col>
+                <Card style={{ width: '25rem', height: '15rem' }}>
+                    <Card.Title>About</Card.Title>
+                    <Card.Text>More About Me</Card.Text>
+                    <Button href='/about' variant="primary">Learn More</Button>
+                </Card>
+            </Col>
+            <Col>
+                <Card style={{ width: '25rem', height: '15rem' }}>
+                    <Card.Title>Projects</Card.Title>
+                    <Card.Text>See Some of My Creations</Card.Text>
+                    <Button href='/about' variant="primary">Learn More</Button>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Card style={{ width: '25rem', height: '15rem' }}>
+                    <Card.Title>Blog</Card.Title>
+                    <Card.Text>Read My Technical Blog Posts</Card.Text>
+                    <Button href='/about' variant="primary">Learn More</Button>
+                </Card>
+            </Col>
+            <Col>
+                <Card style={{ width: '25rem', height: '15rem' }}>
+                    <Card.Title>Contact</Card.Title>
+                    <Card.Text>Get In Touch</Card.Text>
+                    <Button href='/about' variant="primary">Learn More</Button>
+                </Card>
+            </Col>
+        </Row>
+        </Container>
+        </div>
+    )
 }
 
 export default MainContainer;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
-        <div>
-            <Nav>
-                <NavLink to='/' exact>Home</NavLink>   
-                <NavLink to='/about'>About</NavLink>
-                <NavLink to='/projects'>Projects</NavLink>
-                <NavLink to='/blog'>Blog</NavLink>
-                <NavLink to='/contact'>Contact</NavLink>
-            </Nav>
-        </div>
+        <>
+        <Nav className='justify-content-center' defaultActiveKey='/' as="ul">
+            <Nav.Item as="li">
+                <Nav.Link to='/'>Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+                <Nav.Link to='/about'>About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+                <Nav.Link to='/projects'>Projects</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+                <Nav.Link to='/blog'>Blog</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+                <Nav.Link to='/contact'>Contact</Nav.Link>
+            </Nav.Item>
+        </Nav>
+        </>
     )    
 }
 
