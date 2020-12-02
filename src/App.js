3fa04210c91c636f34a5e6d3ed184a889cad5e53
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-12-01 21:51:17 -0700

    added components blog, app, contact, header, proj

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import './App.css';
 import NavBar from './components/NavBar.js'
 import MainContainer from './components/MainContainer.js';
 import About from './components/About.js';
+import Contact from './components/Contact.js';
+import Projects from './components/Projects.js';
+import Blog from './components/Blog.js';
 import { Switch, Route } from 'react-router-dom';
-
+import HeaderImage from './components/HeaderImage.js'
 
 function App() {
   return (
     <div className="App">
       <div id="navbar"><NavBar /></div>
+      <div><HeaderImage /></div>
       <Switch>
         <Route exact path='/' component={MainContainer}/>
         <Route exact path='/about'component={About}/>
+        <Route exact path='/projects'component={Projects}/>
+        <Route exact path='/contact'component={Contact}/>
+        <Route exact path='/blog'component={Blog}/>
       </Switch>
     </div>
   );
---
+++ src/components/HeaderImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+
+const HeaderImage = () => {
+    return(
+        <div className='container'>
+        <Image src='/images/shiba.jpg' fluid/>
+        </div>
+    )
+}
+
+export default HeaderImage;
