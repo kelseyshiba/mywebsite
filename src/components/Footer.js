e858f7868e3a221c6670c48623991570b9fee366
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-12-08 14:40:52 -0700

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Image, Col, Row } from 'react-bootstrap';
 
 const Footer = () => {
     return(
-        <div id='footer'><p>What's Up</p></div>
+        <div id='footer'>
+            <Row>
+            <Col><Image href='https://www.github.com/kelseyshiba' src='/images/github-logo.webp'/></Col>
+            <Col>© Kelsey Shiba 2020</Col>
+            <Col><Image href='https://www.linkedin.com/in/kelsey-shiba' src='/images/linkedin-logo.png'/></Col>
+            </Row>
+        </div>
     )
 }
 
