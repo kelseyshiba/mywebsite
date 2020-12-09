4489f06df80ed89ea5c1b85249301285441b8e0c
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-12-09 16:33:12 -0700

    added videos with react player

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
+import { Col, Row } from 'react-bootstrap';
 
 const Projects = () => {
     return (
-        <div><h1>Projects</h1></div>
+        <div>
+            <h1>Projects</h1><br></br>
+        <Row>
+            <Col>
+                <ReactPlayer url='https://www.youtube.com/watch?v=I0xN36SJCnA'/>
+            </Col>
+            <Col>
+                <ReactPlayer url='https://www.youtube.com/watch?v=bKAk5CGD6nA'/>
+            </Col>
+        </Row><br></br>
+        <Row>
+            <Col>
+                <ReactPlayer url='https://www.youtube.com/watch?v=_P8QUEXldCQ&t=4s'/>
+            </Col>
+            <Col>
+                <ReactPlayer />
+            </Col>
+        </Row>
+        </div>
     )
 }
 
