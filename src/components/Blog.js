6cb30427986dd0d6fc48c8fb015096e350cde4f2
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-12-10 15:22:24 -0700

    added blog button and link

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const Blog = () => {
     return(
-        <div><h1>Blog</h1></div>
+        <div>
+            <h1>Blog</h1>
+            <p>I have lots of content for someone beginning their software engineering journey.
+            I'm also happy to have a few publications available in The Startup!</p>
+            <Button className='cardButton' href='https://www.medium.com/@kelsey-shiba'>Go To Blog</Button>
+        </div>
     )
 }
 
