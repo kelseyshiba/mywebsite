593b4e905a8713769091cd8b17f25043404ce9a9
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-12-10 15:22:40 -0700

    overrode styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,19 +7,19 @@ const NavBar = () => {
         <>
         <Nav className='justify-content-center' defaultActiveKey='/' as="ul">
             <Nav.Item as="li">
-                <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                <Nav.Link className='navlink' as={Link} to='/'>Home</Nav.Link>
             </Nav.Item>
             <Nav.Item as="li">
-                <Nav.Link as={Link} to='/about'>About</Nav.Link>
+                <Nav.Link className='navlink' as={Link} to='/about'>About</Nav.Link>
             </Nav.Item>
             <Nav.Item as="li">
-                <Nav.Link as={Link} to='/projects'>Projects</Nav.Link>
+                <Nav.Link className='navlink' as={Link} to='/projects'>Projects</Nav.Link>
             </Nav.Item>
             <Nav.Item as="li">
-                <Nav.Link as={Link} to='/blog'>Blog</Nav.Link>
+                <Nav.Link className='navlink' as={Link} to='/blog'>Blog</Nav.Link>
             </Nav.Item>
             <Nav.Item as="li">
-                <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
+                <Nav.Link className='navlink' as={Link} to='/contact'>Contact</Nav.Link>
             </Nav.Item>
         </Nav>
         </>
