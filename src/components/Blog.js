36863243f2665ce1a42871bada123f5052ee53aa
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-12-27 14:25:25 -0700

    update blog link

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -8,7 +8,9 @@ const Blog = () => {
             <h1>Blog</h1>
             <p>I have lots of content for someone beginning their software engineering journey.
             I'm also happy to have a few publications available in The Startup!</p>
-            <Button as={Link} className='cardButton' to='https://kelsey-shiba.medium.com' target='_blank'>Go To Blog</Button>
+            <a href='https://kelsey-shiba.medium.com' target='_blank' rel='noreferrer'>
+                <Button className='cardButton'>Go To Blog</Button>
+            </a>
         </div>
     )
 }
