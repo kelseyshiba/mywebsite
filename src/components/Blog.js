e4206007aa609e2ef77125fbc6d437180e0e8115
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-12-28 15:40:57 -0700

    add linked list card to blog page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Card, Col, Row } from 'react-bootstrap';
 
 
 const Blog = () => {
@@ -11,6 +11,25 @@ const Blog = () => {
             <a href='https://kelsey-shiba.medium.com' target='_blank' rel='noreferrer'>
                 <Button className='cardButton'>Go To Blog</Button>
             </a>
+            <h2>Publications</h2>
+            <Row>
+                <Col>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="/images/linkedlists.jpeg" />
+                    <Card.Body>
+                        <Card.Title>Linked Lists for Visual Learners</Card.Title>
+                        <Card.Text>Published by The Startup
+                        </Card.Text>
+                    <a href='https://medium.com/swlh/linked-lists-for-visual-learners-75985f60ce49' target='_blank' rel='noreferrer'>
+                    <Button variant="primary">Read It</Button>
+                    </a>
+                    </Card.Body>
+                </Card>
+                </Col>
+                <Col>
+                    <Card></Card>
+                </Col>
+            </Row>
         </div>
     )
 }
