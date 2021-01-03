baf37e1fdcac58e12daf8f9cb7c8d228186bc7c8
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-02 20:03:54 -0700

    setup new page for design work

---
+++ src/components/Design.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Design = () => {
+    return (
+        <div>
+            <h1>Design Work</h1>
+            <h3>Portfolio Highlights</h3>
+            <Image />
+
+        </div>
+    )
+}
+
+export default Design;
